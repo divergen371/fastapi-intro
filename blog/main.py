30f96bc048f724165f78c5c0d9b44fa83b00838d
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-18 13:53:59 +0900

    :alembic: BaseModelを使わずにデータモデルを定義してみる 結論＝定義できない。必ずPydanicのBaseModelを使うこと

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class Blog:
+    title: str
+    body: str
+
+
 @app.post(path="/blog")
-def create():
-    return "create"
+def create(blog: Blog) -> dict[str, Blog]:
+    return {"data": blog}
