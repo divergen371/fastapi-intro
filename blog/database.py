ee2e1ee19dda7b835e44a7fd52bef9f83447a8e4
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-18 15:02:12 +0900

    :alembic: コードとデータベースの同期セッション autocommitは非推奨でデフォルトでオフ。SQLAlchemyではトランザクションは自動で開始するので。

--- blog/database.py
+++ blog/database.py
@@ -4,11 +4,14 @@ from typing import Any
 # Third Party Library
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./blog.db"
 
 engine: Engine = create_engine(
     url=DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+session_local = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base: Any = declarative_base()
