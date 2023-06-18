2786fbc8260a574a3eb969e47d9ed2bd0fc78f96
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-18 16:18:05 +0900

    :alembic: ユーザーテーブルの定義

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
+# Third Party Library
 from sqlalchemy import Column, Integer, String
+
+# Local Library
 from .database import Base
 
 
 class Blog(Base):
-    __tablename__: str = "users"
+    __tablename__: str = "blogs"
 
     article_id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
