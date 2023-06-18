469e7716b1b17fd4852e61d2b91d807d0b090c50
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-18 16:12:52 +0900

    :alembic: データベーステーブル作成

--- blog/main.py
+++ blog/main.py
@@ -2,9 +2,13 @@
 from fastapi import FastAPI
 
 # Local Library
+from .database import engine
+from .models import Base
 from .schema import Blog
 
 app = FastAPI()
+
+Base.metadata.create_all(engine)
 
 
 @app.post(path="/blog")
