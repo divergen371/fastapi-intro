7aca211c64884378b716cafdfd1476ad27e64329
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-19 21:41:45 +0900

    :alembic: ユーザーIDの自動付与/アクセストークン発行時にメールアドレスとユーザーIDを渡す

--- blog/routes/auth.py
+++ blog/routes/auth.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+# Third Party Library
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+# Local Library
 from .. import models, token
-from ..schema import Login
 from ..database import get_db
 from ..hashing import Hash
-from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["Auth"])
 
@@ -27,5 +29,7 @@ def login(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password"
         )
-    access_token = token.create_access_token(data={"sub": user.email})
+    access_token = token.create_access_token(
+        data={"sub": user.email, "id": user.user_id}
+    )
     return {"access_token": access_token, "token_type": "bearer"}
