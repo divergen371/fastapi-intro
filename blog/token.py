04e2f0e8c7526357ea81497fbc2648b7df6b52ab
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-19 21:37:50 +0900

    :alembic: ユーザーIDの自動付与/データベースセッションを受け取る

--- blog/token.py
+++ blog/token.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from jose import jwt
 from jose.exceptions import JWTError
 from .schema import TokenData
+from sqlalchemy.orm import Session
+from .fanctions.user import show
 
 SECRET_KEY = "86cbacac5b899ec6071f8ded298fdaf805f92d24699b6fc77c7757fe6d6e9b2b"
 ALGORITHM = "HS256"
@@ -10,7 +12,6 @@ ACCESS_TOKE_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    """ """
     to_encode = data.copy()
     if expires_delta:
         expire = expires_delta + datetime.utcnow()
@@ -23,16 +24,18 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encode_jwt
 
 
-def verify_token(token: str, credentials_exception):
+def verify_token(token: str, credentials_exception, db: Session):
     try:
         payload = jwt.decode(
             token=token, key=SECRET_KEY, algorithms=[ALGORITHM]
         )
         email: str = payload.get("sub")
+        user_id: int = payload.get("id")
         if email is None:
             raise credentials_exception
         token_data = TokenData(email=email)
     except JWTError:
         raise credentials_exception
 
-    return token_data
+    user = show(user_id, db)
+    return user
